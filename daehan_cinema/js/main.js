6b918ec05d9e7db031c06d8733b176552e306dcb
notgoodcom <kkun4647@gmail.com>
2017-02-26 13:50:21 +0900

    Add files via upload

---
+++ daehan_cinema/js/main.js
@@ -0,0 +1,43 @@
+'use strict';
+
+
+$(function(){
+	
+	var menu_on = 0;
+	$('#search_down').css('display','none');
+	
+	$(window).scroll(function(event){
+		
+		var scroll_h = $(this).scrollTop();
+	
+		
+		if(scroll_h>menu_on){
+			
+			
+			$('.gnb li a').css('color','#666');
+			$('.gnb li a').css('text-shadow','none');
+			$('.top_menu li a').css('color','#666');
+			$('nav').addClass('nav_on').removeClass('nav_off');	
+			$('#mobile_gnb').css('background','#fff');
+			$('#search').css('display','none');
+			$('#search_down').css('display','block');
+			
+			// scroll-down시 메뉴
+			
+		}else if(scroll_h==0){
+			
+			$('.gnb li a').css('color','#eee');
+			$('.gnb li a').css('text-shadow','1px 1px 2px #555');
+			$('.top_menu li a').css('color','#ddd');
+			$('nav').removeClass('nav_on').addClass('nav_off');
+			$('#mobile_gnb').css('background','rgba(0,0,0,0.2)');
+			$('#search').css('display','block');
+			$('#search_down').css('display','none');
+			
+			//// scroll-up시 메뉴
+			
+		}
+		menu_on = scroll_h; 
+	});
+	
+});
